0a7b85f2714971febd80522f7535d07d4a325742
Przemysław Solan <przemek.solan@mycit.ie>
2018-11-06 14:27:28 +0000

    Added POSTMAN api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var admin = require('firebase-admin');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,7 @@ var firebaseAdmin = admin.initializeApp({
 var database = firebaseAdmin.database();
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +34,15 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dashboard', dashboardRouter);
+
+bodyParser = require('body-parser').json();
+app.post('/test', function(req, res) {
+    //var Keywords = req.body.Keywords;
+    console.log("Yoooooo");
+    console.log(req.headers);
+    console.log(req.body);
+    res.status(200).send("yay");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', function(req, res) {
+    //var Keywords = req.body.Keywords;
+    console.log("Yoooooo");
+    console.log(req.headers);
+    console.log(req.body);
+    res.status(200).send("yay");
 });
 
 module.exports = router;
